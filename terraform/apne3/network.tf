8a248ed250185edd2a2c454acb1f70f2d6aa2570
ogadra <61941819+ogadra@users.noreply.github.com>
2026-06-07 13:57:50 +0900

    refactor: colocate regional peering variables

--- terraform/apne3/network.tf
+++ terraform/apne3/network.tf
@@ -1,3 +1,13 @@
+variable "peer_vpc_cidr" {
+  description = "CIDR block of the peer VPC reachable through VPC peering"
+  type        = string
+}
+
+variable "vpc_peering_connection_id" {
+  description = "ID of the accepted VPC peering connection used for private subnet routes"
+  type        = string
+}
+
 resource "aws_vpc" "apne3" {
   cidr_block           = local.vpc_cidr
   enable_dns_hostnames = true
--- terraform/apne3/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "peer_vpc_cidr" {
-  description = "CIDR block of the peer VPC reachable through VPC peering"
-  type        = string
-}
-
-variable "vpc_peering_connection_id" {
-  description = "ID of the accepted VPC peering connection used for private subnet routes"
-  type        = string
-}
