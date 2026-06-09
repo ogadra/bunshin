4b6c7153fda18b73ec3a7784b626df29a2d544a1
ogadra <61941819+ogadra@users.noreply.github.com>
2026-06-10 08:35:28 +0900

    refactor: add validation for runner_desired_count

--- terraform/apne3/variables.tf
+++ terraform/apne3/variables.tf
@@ -1,4 +1,9 @@
 variable "runner_desired_count" {
   description = "Desired number of runner ECS tasks"
   type        = number
+
+  validation {
+    condition     = var.runner_desired_count >= 0 && floor(var.runner_desired_count) == var.runner_desired_count
+    error_message = "runner_desired_count must be a non-negative integer."
+  }
 }
