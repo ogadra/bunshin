f89bd616296bcd1671bbca10f921f01b3aa1d142
ogadra <61941819+ogadra@users.noreply.github.com>
2026-06-14 21:25:25 +0900

    feat(terraform): add static asset regional dr

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,13 @@
 module "apne1" {
   source = "./apne1"
 
-  alb_certificate_arn  = var.alb_certificate_arns.apne1
-  bunshin_stacks       = local.bunshin_stacks
-  domain_name          = var.domain_name
-  runner_desired_count = var.runner_desired_count
+  alb_certificate_arn                                     = var.alb_certificate_arns.apne1
+  bunshin_stacks                                          = local.bunshin_stacks
+  cloudfront_distribution_arn                             = aws_cloudfront_distribution.main.arn
+  domain_name                                             = var.domain_name
+  runner_desired_count                                    = var.runner_desired_count
+  static_replication_destination_bucket_arn               = module.apne3.static_bucket_arn
+  static_replication_destination_bucket_versioning_status = module.apne3.static_bucket_versioning_status
 
   providers = {
     aws = aws.apne1
@@ -14,10 +17,11 @@ module "apne1" {
 module "apne3" {
   source = "./apne3"
 
-  alb_certificate_arn  = var.alb_certificate_arns.apne3
-  bunshin_stacks       = local.bunshin_stacks
-  domain_name          = var.domain_name
-  runner_desired_count = var.runner_desired_count
+  alb_certificate_arn         = var.alb_certificate_arns.apne3
+  bunshin_stacks              = local.bunshin_stacks
+  cloudfront_distribution_arn = aws_cloudfront_distribution.main.arn
+  domain_name                 = var.domain_name
+  runner_desired_count        = var.runner_desired_count
 
   providers = {
     aws = aws.apne3
