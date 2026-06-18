4bb7e73065a8f6b0e445ef411490afd27ea5a99b
ogadra <61941819+ogadra@users.noreply.github.com>
2026-06-18 09:31:25 +0900

    chore(terraform): remove unused apne1 static outputs

--- terraform/apne1/outputs.tf
+++ terraform/apne1/outputs.tf
@@ -53,17 +53,7 @@ output "nginx_security_group_id" {
   value       = aws_security_group.nginx.id
 }
 
-output "static_bucket_arn" {
-  description = "ARN of the static asset bucket"
-  value       = aws_s3_bucket.static.arn
-}
-
 output "static_bucket_regional_domain_name" {
   description = "Regional domain name of the static asset bucket"
   value       = aws_s3_bucket.static.bucket_regional_domain_name
-}
-
-output "static_bucket_versioning_status" {
-  description = "Versioning status of the static asset bucket"
-  value       = aws_s3_bucket_versioning.static.versioning_configuration[0].status
 }
