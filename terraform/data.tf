5c6877fc23badcc94e79d300b26be7b8e582853a
ogadra <61941819+ogadra@users.noreply.github.com>
2026-06-21 14:13:09 +0900

    refactor: move certificate data sources to data tf

--- terraform/data.tf
+++ terraform/data.tf
@@ -7,3 +7,27 @@ data "aws_region" "apne1" {
 data "aws_region" "apne3" {
   provider = aws.apne3
 }
+
+data "aws_acm_certificate" "cloudfront" {
+  provider = aws.use1
+
+  domain      = var.domain_name
+  statuses    = ["ISSUED"]
+  most_recent = true
+}
+
+data "aws_acm_certificate" "apne1_alb" {
+  provider = aws.apne1
+
+  domain      = var.domain_name
+  statuses    = ["ISSUED"]
+  most_recent = true
+}
+
+data "aws_acm_certificate" "apne3_alb" {
+  provider = aws.apne3
+
+  domain      = var.domain_name
+  statuses    = ["ISSUED"]
+  most_recent = true
+}
