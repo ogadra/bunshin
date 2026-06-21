a3007da0b471aadb8561c3945a2f6cb597cd1009
ogadra <61941819+ogadra@users.noreply.github.com>
2026-06-21 11:26:14 +0900

    fix: constrain static replication assume role

--- terraform/apne1/s3.tf
+++ terraform/apne1/s3.tf
@@ -77,6 +77,18 @@ data "aws_iam_policy_document" "static_replication_assume_role" {
       type        = "Service"
       identifiers = ["s3.amazonaws.com"]
     }
+
+    condition {
+      test     = "StringEquals"
+      variable = "aws:SourceAccount"
+      values   = [data.aws_caller_identity.current.account_id]
+    }
+
+    condition {
+      test     = "ArnEquals"
+      variable = "aws:SourceArn"
+      values   = [aws_s3_bucket.static.arn]
+    }
   }
 }
 
