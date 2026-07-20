c478d48310c93ed61e460329d5a43c0503c445f9
ogadra <61941819+ogadra@users.noreply.github.com>
2026-07-21 01:02:41 +0900

    fix for deploy

--- scripts/aws/deploy/front.sh
+++ scripts/aws/deploy/front.sh
@@ -7,9 +7,12 @@ main() {
     local env_name="${1:?Usage: scripts/aws/deploy/front.sh <env> <aws_account_id>}"
     local aws_account_id="${2:?Usage: scripts/aws/deploy/front.sh <env> <aws_account_id>}"
     local bucket_name="bunshin-static-${aws_account_id}-ap-northeast-1-an"
+    local domain
+    domain="$(terraform -chdir="${ROOT_DIR}/terraform/aws" output -raw domain_name)"
 
     echo "Deploying front to ${env_name}"
-    pnpm --dir "${ROOT_DIR}/front" build
+    VITE_PERL_ORIGIN_TEMPLATE="https://{hex}.{stack}.${domain}/" \
+        pnpm --dir "${ROOT_DIR}/front" build
 
     aws --profile "${env_name}" --region ap-northeast-1 s3 sync \
         "${ROOT_DIR}/front/dist/" \
